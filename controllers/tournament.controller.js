c7aecbf4485d4f1e8f789421312024c6cd62eb44
Pablo Martínez <pablomartinez1017@gmail.com>
2021-06-02 18:18:13 +0200

    custom tournament queries

--- controllers/tournament.controller.js
+++ controllers/tournament.controller.js
@@ -1,33 +1,50 @@
-const db = require("../models")
+const db = require("../models");
 const Tournament = db.tournament;
 const Op = db.Sequelize.Op;
+const moment = require("moment");
 
 exports.findOne = (req, res) => {
   const id = req.params.id;
 
-  User.findByPk(id)
-    .then(data => {
+  Tournament.findByPk(id)
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: "Error retrieving Tournament with id=" + id
+        message: "Error retrieving Tournament with id=" + id,
       });
     });
 };
 
 exports.findAll = (req, res) => {
   const id = req.query.id;
-  var condition = id ? { name: { [Op.like]: '%${id}%'} } : null;
+  var condition = id ? { id: { [Op.like]: "%${id}%" } } : null;
 
-  Tournament.findAll({ where: condition})
-    .then(data => {
+  Tournament.findAll()
+    .then((data) => {
       res.send(data);
     })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving tournaments."
-        });
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tournaments.",
       });
+    });
+};
+
+exports.findAllByYear = (req, res) => {
+  const dateTop = req.query.yearTop;
+  const dateBot = req.query.yearBot;
+
+  Tournament.findAll({ where: {date: {[Op.gte]: dateTop, [Op.lte]: dateBot}} })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tournaments.",
+      });
+    });
 };
